e4dd3fd5b791bc660f8574b7e94d395250fa92d0
Ghada Mohamed <ghada.mohamed.abdelrazek@gmail.com>
2025-11-23 16:27:22 +0200

    add file task

---
+++ lab.js
@@ -0,0 +1,22 @@
+console.log ( res.toPrecision(6))
+
+console.log (res1);
+console.log (res2);
+
+// var value1 = prompt("Enter the Frist value")
+// var value2 = prompt("Enter the second value")
+
+console.log("As String " + value1 + value2)
+console.log ( Number(value1)+ Number(value2))
+
+var num1 = prompt("Enter the Frist Number")
+var num2 = prompt("Enter the second Number")
+var num3 = prompt("Enter the Thrid Number")
+var num4 = prompt("Enter the fourth Number")
+var num5 = prompt("Enter the fifth Number")
+
+console.log (num1)
+console.log (num2)
+console.log (num3)
+console.log (num4)
+console.log (num5)
